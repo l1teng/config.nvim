96578ccea053e62743f64b92c199deca9df15f19
Teng Li <tengli.terry+dev@gmail.com>
2022-10-13 14:07:54 +0800

    feat(lsp): update lsp list
    Signed-off-by: Teng Li <tengli.terry+dev@gmail.com>

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -7,17 +7,31 @@ mason.setup()
 local servers = {
   sumneko_lua = require "lspconfig.server_configurations.sumneko_lua",
   clangd = require "lspconfig.server_configurations.clangd",
+  cmake = require "lspconfig.server_configurations.cmake",
   texlab = require "lspconfig.server_configurations.texlab",
   pyright = require "lspconfig.server_configurations.pyright",
-  -- jedi_language_server = require "lspconfig.server_configurations.jedi_language_server",
   yamlls = require "lspconfig.server_configurations.yamlls",
   zk = require "lspconfig.server_configurations.zk",
   bashls = require "lspconfig.server_configurations.bashls",
   html = require "lspconfig.server_configurations.html",
+  cssls = require "lspconfig.server_configurations.cssls",
   jsonls = require "lspconfig.server_configurations.jsonls",
+  dockerls = require "lspconfig.server_configurations.dockerls",
+  jdtls = require "lspconfig.server_configurations.jdtls",
+  gopls = require "lspconfig.server_configurations.gopls",
+  taplo = require "lspconfig.server_configurations.taplo",
 }
+
+local _servers = function(t)
+  local keys={}
+  for key,_ in pairs(t) do
+    table.insert(keys, key)
+  end
+  return keys
+end
+
 mason_lspconfig.setup({
-  ensure_installed = { "sumneko_lua", "pyright" }
+  ensure_installed = _servers(servers),
 })
 mason_lspconfig.setup_handlers({
   function(server)
